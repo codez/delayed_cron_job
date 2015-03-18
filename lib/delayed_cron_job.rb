b7ede5879f835982a1f990148daa8b0faba824c1
Caio SBA <caiosba@gmail.com>
2015-03-18 14:34:52 -0300

    Set cron to be an accessible attribute

--- lib/delayed_cron_job.rb
+++ lib/delayed_cron_job.rb
@@ -10,6 +10,11 @@ end
 
 if defined?(Delayed::Backend::Mongoid)
   Delayed::Backend::Mongoid::Job.field :cron, :type => String
+  Delayed::Backend::Mongoid::Job.attr_accessible(:cron)
+end
+
+if defined?(Delayed::Backend::ActiveRecord)
+  Delayed::Backend::ActiveRecord::Job.attr_accessible(:cron)
 end
 
 DelayedCronJob::Plugin.callback_block.call(Delayed::Worker.lifecycle)
