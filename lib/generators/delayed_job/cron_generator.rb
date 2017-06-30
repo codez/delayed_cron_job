0f583823079aae72a2c8dc4cc15afbd208d90092
Pascal Zumkehr <pascal@codez.ch>
2017-06-30 19:55:03 +0200

    change migration template to include version if required

--- lib/generators/delayed_job/cron_generator.rb
+++ lib/generators/delayed_job/cron_generator.rb
@@ -12,11 +12,22 @@ module DelayedJob
     self.source_paths << File.join(File.dirname(__FILE__), 'templates')
 
     def create_migration_file
-      migration_template('cron_migration.rb', 'db/migrate/add_cron_to_delayed_jobs.rb')
+      migration_template('cron_migration.rb',
+                         'db/migrate/add_cron_to_delayed_jobs.rb',
+                         migration_version: migration_version)
     end
 
     def self.next_migration_number(dirname)
       ActiveRecord::Generators::Base.next_migration_number(dirname)
     end
+
+    private
+
+    def migration_version
+      if ActiveRecord::VERSION::MAJOR >= 5
+        "[#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}]"
+      end
+    end
+
   end
 end
--- lib/generators/delayed_job/templates/cron_migration.rb
+++ lib/generators/delayed_job/templates/cron_migration.rb
@@ -1,4 +1,4 @@
-class AddCronToDelayedJobs < ActiveRecord::Migration[4.2]
+class AddCronToDelayedJobs < ActiveRecord::Migration<%= migration_version %>
   def self.up
     add_column :delayed_jobs, :cron, :string
   end
